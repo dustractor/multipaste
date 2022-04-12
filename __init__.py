ea0a0f9d4d417c990f7a92c9d96ef2f8ee1013a8
Shams Kitz <dustractor@gmail.com>
2022-04-12 00:01:33 -0500

    linux-only fttb

--- __init__.py
+++ __init__.py
@@ -13,69 +13,34 @@ def _(a=None,b=[]):
     else:
         return b
 
-@_
-class MultiPastePrefs(bpy.types.AddonPreferences):
-    bl_idname = __package__
-    def draw(self,context):
-        self.layout.operator("multipaste.install_module")
-@_
-class MULTIPASTE_OT_install_module(bpy.types.Operator):
-    bl_idname = "multipaste.install_module"
-    bl_label = "install modules"
-    module_name: bpy.props.StringProperty(default="pywin32")
-    def execute(self,context):
-        import subprocess
-        import ensurepip
-        import sys
-        ensurepip.bootstrap()
-        python_binary = sys.executable
-        t = subprocess.check_call([python_binary,"-m","pip","install",self.module_name])
-        print("t:",t)
-        return {"FINISHED"}
 
 @_
 class MULTIPASTE_OT_multipaste(bpy.types.Operator):
     bl_idname = "multipaste.multipaste"
     bl_label = "multi paste operator"
     def execute(self,context):
-        try:
-            import win32clipboard
-        except ImportError:
-            print("is pywin32 installed? (try doing that)")
-        _list = []
-        win32clipboard.OpenClipboard()
-        try:
-            text = win32clipboard.GetClipboardData()
-            _list += [text]
-
-        except TypeError:
-            _list = win32clipboard.GetClipboardData(win32clipboard.CF_HDROP)
-        finally:
-            win32clipboard.CloseClipboard()
-        for text in _list:
-            try:
-                exists = pathlib.Path(text).exists()
-            except OSError:
-                print("The following clipboard content does not appear to be a list of files:")
-                print("-------->")
-                print(text)
-                print("<--------")
-            if text.endswith(".svg"):
-                bpy.ops.import_curve.svg(filepath=text)
-            elif text.endswith(".obj"):
-                bpy.ops.import_scene.obj(filepath=text)
-            elif text.endswith(".dae"):
-                bpy.ops.wm.collada_import(filepath=text)
-            elif text.endswith(".ply"):
-                bpy.ops.import_mesh.ply(filepath=text)
-            elif text.endswith(".stl"):
-                bpy.ops.import_mesh.stl(filepath=text)
+        paths = []
+        for f in context.window_manager.clipboard.splitlines():
+            if f.startswith("file://"):
+                f = f[7:]
+                if pathlib.Path(f).is_file():
+                    paths.append(f)
+        print("multipaste paths found:",paths)
+        for path in paths:
+            if path.endswith(".svg"):
+                bpy.ops.import_curve.svg(filepath=path)
+            elif path.endswith(".obj"):
+                bpy.ops.import_scene.obj(filepath=path)
+            elif path.endswith(".dae"):
+                bpy.ops.wm.collada_import(filepath=path)
+            elif path.endswith(".ply"):
+                bpy.ops.import_mesh.ply(filepath=path)
+            elif path.endswith(".stl"):
+                bpy.ops.import_mesh.stl(filepath=path)
             else:
                 print("recieved a file whose extension has no handler:")
-                print(text)
-
-
-        print("ok")
+                print("path:",path)
+        print("multipaste ok")
         return {"FINISHED"}
 
 
